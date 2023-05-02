5a779538707f639053892183a3731d04cc5ec414
Vicky <vickymendoza90@gmail.com>
2023-05-01 19:10:48 -0500

    aquetando los test

---
+++ src/test/09-useContext/HomePage.test.jsx
@@ -0,0 +1,42 @@
+import { render, screen } from '@testing-library/react';
+import { UserContext } from '../../src/09-useContext/context/UserContext';
+import { HomePage } from '../../src/09-useContext/HomePage';
+
+
+describe('Pruebas en <HomePage />', () => {
+
+    const user = {
+        id: 1,
+        name: 'Victoria'
+    }
+
+    test('debe de mostrar el componente sin el usuario', () => {
+
+        render(
+            <UserContext.Provider value={{ user: null }}>
+                <HomePage />
+            </UserContext.Provider>
+        );
+
+        const preTag = screen.getByLabelText('pre'); // aria-label
+        expect(preTag.innerHTML).toBe('null');
+        // screen.debug()
+    });
+
+
+    test('debe de mostrar el componente CON el usuario', () => {
+
+        render(
+            <UserContext.Provider value={{ user }}>
+                <HomePage />
+            </UserContext.Provider>
+        );
+
+        const preTag = screen.getByLabelText('pre'); // aria-label
+        expect(preTag.innerHTML).toContain(user.name);
+        expect(preTag.innerHTML).toContain(`${user.id}`);
+        //imprime lo que está en es escribe
+        // screen.debug()
+    });
+
+});
